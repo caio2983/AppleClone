c422f46de4c8ed3b8fde859388d33a2e99b5f6d7
caio2983 <caioluna2002@gmail.com>
2024-11-14 17:52:10 -0300

    refactor: create HolidayBlock component and move components to specific folders

---
+++ src/app/components/HolidayBlock/holiday.js
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+export default function HolidayBlock() {
+  return (
+    <div className="h-[85vh] w-auto flex-col bg-red-400 py-[55px] flex items-center justify-end overflow-hidden relative">
+      <div
+        id="img-wrapper"
+        className=" w-full  h-full top-0 left-0 absolute bg-slate-700"
+      >
+        <video
+          src="apple_vid_holiday.mp4"
+          className="scale-[1.55] absolute top-[100px]"
+          autoPlay
+          loop={false}
+          muted
+          disablePictureInPicture
+        ></video>
+      </div>
+
+      <div
+        id="content-wrapper"
+        className="flex flex-col items-center text-black justify-self-end z-10 relative bottom-[80px] gap-[8px] "
+      >
+        <h2 className="headline">Gift magic.</h2>
+        <p>Make their holiday wish come true.</p>
+        <button>Shop Gifts</button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,37 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import Header from "./components/header";
+import Header from "./components/Header/header";
+import HolidayBlock from "./components/HolidayBlock/holiday";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-col w-[100vw] max-w-[100vw]">
       <Header></Header>
-      <div className="h-[85vh] w-auto flex-col bg-red-400 py-[55px] flex items-center justify-end overflow-hidden relative">
-        <div
-          id="img-wrapper"
-          className=" w-full  h-full top-0 left-0 absolute bg-slate-700"
-        >
-          <video
-            src="apple_vid_holiday.mp4"
-            className="scale-[1.55] absolute top-[100px]"
-            autoPlay
-            loop={false}
-            muted
-            disablePictureInPicture
-          ></video>
-        </div>
-
-        <div
-          id="content-wrapper"
-          className="flex flex-col items-center text-black justify-self-end z-10 relative bottom-[80px] gap-[8px] "
-        >
-          <h2 className="headline">Gift magic.</h2>
-          <p>Make their holiday wish come true.</p>
-          <button>Shop Gifts</button>
-        </div>
-      </div>
+      <HolidayBlock></HolidayBlock>
     </div>
   );
 }
