7209b1f233b02e129e225ac06fbf1fa986d1fcfe
caio2983 <caioluna2002@gmail.com>
2024-11-15 15:53:08 -0300

    feat: create iphone block component

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,19 @@
 import Image from "next/image";
 import Header from "./components/Header/header";
 import HolidayBlock from "./components/HolidayBlock/holiday";
+import IphoneBlock from "./components/IphoneBlock/iphone";
 import { useEffect, useState } from "react";
 import React from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-col w-[100vw] max-w-[100vw]">
+    <div className="  max-w-[100vw] ">
       <Header></Header>
       <HolidayBlock></HolidayBlock>
+      <div id="iphone-blocks" className="flex flex-col gap-[16px]">
+        <IphoneBlock></IphoneBlock>
+        <IphoneBlock></IphoneBlock>
+      </div>
     </div>
   );
 }
