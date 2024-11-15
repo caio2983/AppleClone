6e015427c35c85040725c74499e65d4f773864c5
caio2983 <caioluna2002@gmail.com>
2024-11-15 15:54:45 -0300

    styles: update holiday block styles

--- src/app/components/HolidayBlock/holiday.js
+++ src/app/components/HolidayBlock/holiday.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
 import React from "react";
+import { useEffect } from "react";
 
 export default function HolidayBlock() {
   return (
-    <div className="h-[85vh] w-auto flex-col bg-red-400 py-[55px] flex items-center justify-end overflow-hidden relative">
-      <div
-        id="img-wrapper"
-        className=" w-full  h-full top-0 left-0 absolute bg-slate-700"
-      >
+    <div
+      id="hero"
+      className="h-[80vh] w-auto flex-col pt-[55px] flex items-center justify-end overflow-hidden relative"
+    >
+      <div id="img-wrapper" className=" w-full  top-0 left-0 absolute">
         <video
           src="apple_vid_holiday.mp4"
           className="scale-[1.55] absolute top-[100px]"
@@ -21,10 +22,13 @@ export default function HolidayBlock() {
 
       <div
         id="content-wrapper"
-        className="flex flex-col items-center text-black justify-self-end z-10 relative bottom-[80px] gap-[8px] "
+        className="flex flex-col items-center text-black justify-self-end z-10 absolute bottom-[80px] gap-[32px] "
       >
-        <h2 className="headline">Gift magic.</h2>
-        <p>Make their holiday wish come true.</p>
+        <span className="flex flex-col items-center gap-[8px]">
+          <h2 className="headline">Gift magic.</h2>
+          <p>Make their holiday wish come true.</p>
+        </span>
+
         <button>Shop Gifts</button>
       </div>
     </div>
