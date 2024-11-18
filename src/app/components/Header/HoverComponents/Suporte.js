8dd407469b79d4c2a95177a0001a90785d489e94
caio2983 <caioluna2002@gmail.com>
2024-11-18 19:21:24 -0300

    feat: update suporte hover component

--- src/app/components/Header/HoverComponents/Suporte.js
+++ src/app/components/Header/HoverComponents/Suporte.js
@@ -4,10 +4,13 @@ export default function SuporteHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Comprar</h2>
+        <h2 className="submenu-header">Explorar suporte</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
             <a className="nav-submenu-link">Comprar os Lançamentos</a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-submenu-link">Iphone</a>
           </li>
           <li className="nav-list-item">
             <a className="nav-submenu-link">Mac</a>
@@ -16,43 +19,47 @@ export default function SuporteHoverComponent() {
             <a className="nav-submenu-link">Ipad</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Iphone</a>
-          </li>
-          <li className="nav-list-item">
             <a className="nav-submenu-link">Apple Watch</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Acessórios</a>
+            <a className="nav-submenu-link">AirPods</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Links rápidos</h2>
+        <h2 className="submenu-header">Ajuda</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar presentes de Natal</a>
+            <a className="nav-submenu-link">Comunidade</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Encontre uma loja</a>
+            <a className="nav-submenu-link">Verificar cobertura</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Status do pedido</a>
+            <a className="nav-submenu-link">Reparos</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Financiamento</a>
+            <a className="nav-submenu-link">Fale com a gente</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar nas lojas especiais</h2>
+        <h2 className="submenu-header">Tópicos úteis</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
+            <a className="nav-submenu-link">Comprar produtos AppleCare</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Negócios</a>
+            <a className="nav-submenu-link">Conta Apple e senha</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Cobrança e assinaturas</a>
+          </li>
+
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Acessibilidade</a>
           </li>
         </ul>
       </div>
