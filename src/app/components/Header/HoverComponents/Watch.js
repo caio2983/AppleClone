b7c19a3312ddcdcc63eb807e4cb8728bb9c85dbe
caio2983 <caioluna2002@gmail.com>
2024-11-18 17:00:54 -0300

    feat: update watch hover component

--- src/app/components/Header/HoverComponents/Watch.js
+++ src/app/components/Header/HoverComponents/Watch.js
@@ -4,40 +4,39 @@ export default function WatchHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Comprar</h2>
+        <h2 className="submenu-header">Explorar Apple Watch</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Comprar os Lançamentos</a>
+            <a className="nav-submenu-link">
+              Explorar todos os modelos de Apple Watch
+            </a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Mac</a>
+            <a className="nav-submenu-link">Apple Watch Series 10</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Ipad</a>
+            <a className="nav-submenu-link">Apple Watch Ultra 2</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Iphone</a>
+            <a className="nav-submenu-link">Apple Watch SE</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Apple Watch</a>
-          </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Acessórios</a>
+            <a className="nav-submenu-link">Apple Watch Nike</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Links rápidos</h2>
+        <h2 className="submenu-header">Comprar Apple Watch</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar presentes de Natal</a>
+            <a className="nav-submenu-link">Comprar Apple Watch</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Encontre uma loja</a>
+            <a className="nav-submenu-link">Pulseiras para o Apple Watch</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Status do pedido</a>
+            <a className="nav-submenu-link">Acessórios para o Apple Watch</a>
           </li>
           <li className="nav-list-item-smaller">
             <a className="nav-submenu-link">Financiamento</a>
@@ -46,13 +45,19 @@ export default function WatchHoverComponent() {
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar nas lojas especiais</h2>
+        <h2 className="submenu-header">Mais para o Apple Watch</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
+            <a className="nav-submenu-link">Suporte para o Apple Watch</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Negócios</a>
+            <a className="nav-submenu-link">watchOS 11</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Apps da Apple</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Apple Fitness+</a>
           </li>
         </ul>
       </div>
