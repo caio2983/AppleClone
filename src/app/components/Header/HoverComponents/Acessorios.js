5befabfdf987be015329f61b772acc549d7aa81c
caio2983 <caioluna2002@gmail.com>
2024-11-18 19:18:32 -0300

    feat: update acessorios hover component

--- src/app/components/Header/HoverComponents/Acessorios.js
+++ src/app/components/Header/HoverComponents/Acessorios.js
@@ -4,10 +4,10 @@ export default function AcessoriosHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Comprar</h2>
+        <h2 className="submenu-header">Comprar acessórios</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Comprar os Lançamentos</a>
+            <a className="nav-submenu-link">Ver todos acessórios</a>
           </li>
           <li className="nav-list-item">
             <a className="nav-submenu-link">Mac</a>
@@ -21,38 +21,20 @@ export default function AcessoriosHoverComponent() {
           <li className="nav-list-item">
             <a className="nav-submenu-link">Apple Watch</a>
           </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Acessórios</a>
-          </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Links rápidos</h2>
+        <h2 className="submenu-header">Explorar acessórios</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar presentes de Natal</a>
+            <a className="nav-submenu-link">Criados pela Apple</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Encontre uma loja</a>
+            <a className="nav-submenu-link">Beats bt Dr. Dre</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Status do pedido</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Financiamento</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar nas lojas especiais</h2>
-        <ul className="nav-list">
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Negócios</a>
+            <a className="nav-submenu-link">AirTag</a>
           </li>
         </ul>
       </div>
