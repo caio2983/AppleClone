e7cc2766ccadb688838bdd11fc877ce010d730a3
caio2983 <caioluna2002@gmail.com>
2024-11-18 19:10:11 -0300

    feat: update airpods hover component

--- src/app/components/Header/HoverComponents/Airpods.js
+++ src/app/components/Header/HoverComponents/Airpods.js
@@ -4,55 +4,45 @@ export default function AirpodsHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Comprar</h2>
+        <h2 className="submenu-header">Explorar AirPods</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Comprar os Lançamentos</a>
+            <a className="nav-submenu-link">
+              Explorar todos os modelos de AirPods
+            </a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Mac</a>
+            <a className="nav-submenu-link">AirPods 4</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Ipad</a>
+            <a className="nav-submenu-link">AirPods Pro 2</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Iphone</a>
-          </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Apple Watch</a>
-          </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Acessórios</a>
+            <a className="nav-submenu-link">AirPods Max</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Links rápidos</h2>
+        <h2 className="submenu-header">Comprar AirPods</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar presentes de Natal</a>
+            <a className="nav-submenu-link">Comprar AirPods</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Encontre uma loja</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Status do pedido</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Financiamento</a>
+            <a className="nav-submenu-link">Acessórios para AirPods</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar nas lojas especiais</h2>
+        <h2 className="submenu-header">Mais para os AirPods</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
+            <a className="nav-submenu-link">Suporte para AirPods</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Negócios</a>
+            <a className="nav-submenu-link">Apple Music</a>
           </li>
         </ul>
       </div>
