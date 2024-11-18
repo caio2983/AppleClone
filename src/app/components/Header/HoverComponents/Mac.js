b6a215b8bb58425bdd5e2193964bde16a76c4b72
caio2983 <caioluna2002@gmail.com>
2024-11-18 16:23:25 -0300

    feat: update Mac hover component

--- src/app/components/Header/HoverComponents/Mac.js
+++ src/app/components/Header/HoverComponents/Mac.js
@@ -4,40 +4,43 @@ export default function MacHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Comprar</h2>
+        <h2 className="submenu-header">Explorar Mac</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Comprar os Lançamentos</a>
+            <a className="nav-submenu-link">Explorar todos os modelos de Mac</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Mac</a>
+            <a className="nav-submenu-link">MacBook Air</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Ipad</a>
+            <a className="nav-submenu-link">MacBook Pro</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Iphone</a>
+            <a className="nav-submenu-link">iMac</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Apple Watch</a>
+            <a className="nav-submenu-link">Mac mini</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">Acessórios</a>
+            <a className="nav-submenu-link">Mac Studio</a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-submenu-link">Mac Pro</a>
+          </li>
+          <li className="nav-list-item">
+            <a className="nav-submenu-link">Monitores</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Links rápidos</h2>
+        <h2 className="submenu-header">Comprar Mac</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar presentes de Natal</a>
+            <a className="nav-submenu-link">Comprar Mac</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Encontre uma loja</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Status do pedido</a>
+            <a className="nav-submenu-link">Acessórios para Mac</a>
           </li>
           <li className="nav-list-item-smaller">
             <a className="nav-submenu-link">Financiamento</a>
@@ -46,13 +49,34 @@ export default function MacHoverComponent() {
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar nas lojas especiais</h2>
+        <h2 className="submenu-header">Mais para o Mac</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
+            <a className="nav-submenu-link">Suporte para o Mac</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Negócios</a>
+            <a className="nav-submenu-link">Produtos AppleCare</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">macOS Sequoia</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Apple intelligence</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Apps da Apple</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Continuidade</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">iCloud+</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Mac nos Negócios</a>
+          </li>
+          <li className="nav-list-item-smaller">
+            <a className="nav-submenu-link">Educação</a>
           </li>
         </ul>
       </div>
