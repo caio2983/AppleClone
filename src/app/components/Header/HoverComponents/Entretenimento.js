2cd046564c5d0eeb7d9c5ef7249d1ec5869ddbab
caio2983 <caioluna2002@gmail.com>
2024-11-18 19:16:03 -0300

    feat: update entretenimento hover component

--- src/app/components/Header/HoverComponents/Entretenimento.js
+++ src/app/components/Header/HoverComponents/Entretenimento.js
@@ -4,73 +4,34 @@ export default function EntretenimentoHoverComponent() {
   return (
     <div className="w-full h-full flex ml-[256px] flyout-content">
       <div className="flex flex-col pr-[88px]">
-        <h2 className="submenu-header">Explorar iPad</h2>
+        <h2 className="submenu-header">Explorar Entretenimento</h2>
         <ul className="nav-list">
           <li className="nav-list-item">
-            <a className="nav-submenu-link">
-              Explorar todos os modelos de iPad
-            </a>
+            <a className="nav-submenu-link">Explorar Entretenimento</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">iPad Pro</a>
+            <a className="nav-submenu-link">Apple One</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">iPad Air</a>
+            <a className="nav-submenu-link">Apple TV+</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">iPad</a>
+            <a className="nav-submenu-link">Apple Music</a>
           </li>
           <li className="nav-list-item">
-            <a className="nav-submenu-link">iPad Mini</a>
-          </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Apple pencil</a>
-          </li>
-          <li className="nav-list-item">
-            <a className="nav-submenu-link">Teclado</a>
+            <a className="nav-submenu-link">Apple One Arcade</a>
           </li>
         </ul>
       </div>
 
       <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Comprar iPad</h2>
+        <h2 className="submenu-header">Suporte</h2>
         <ul className="nav-list">
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Comprar iPad</a>
+            <a className="nav-submenu-link">Suporte para Apple TV+</a>
           </li>
           <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Acessórios para o iPad</a>
-          </li>
-
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Financiamento</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex flex-col pr-[44px]">
-        <h2 className="submenu-header">Mais para o iPad</h2>
-        <ul className="nav-list">
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Suportes para o </a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
-          </li>
-          <li className="nav-list-item-smaller">
-            <a className="nav-submenu-link">Educação</a>
+            <a className="nav-submenu-link">Suporte para Apple Music</a>
           </li>
         </ul>
       </div>
