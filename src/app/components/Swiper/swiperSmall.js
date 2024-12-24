678ddd919a4edb6a635c8eda8407340013571faa
caio2983 <caioluna2002@gmail.com>
2024-12-24 12:26:32 -0300

    feat: add mouse over functionality in small swiper

--- src/app/components/Swiper/swiperSmall.js
+++ src/app/components/Swiper/swiperSmall.js
@@ -1,23 +1,15 @@
-import React from "react";
-import {
-  Navigation,
-  Pagination,
-  Scrollbar,
-  EffectCoverflow,
-  Parallax,
-  Grid,
-  Autoplay,
-} from "swiper/modules";
-
+import React, { useState } from "react";
+import { Navigation, Scrollbar, Grid, Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
 export default function SwiperSmallComponent() {
+  const [swiperInstance, setSwiperInstance] = useState(null);
+
   const slides = [
     { img: "/slides/slidesSmall/HelloKitty.jpg", text: [] },
     { img: "/slides/slidesSmall/HelloKitty.jpg", text: [] },
@@ -31,34 +23,52 @@ export default function SwiperSmallComponent() {
     { img: "/slides/slidesSmall/HelloKitty.jpg", text: [] },
   ];
 
+  const handleMouseOver = () => {
+    if (swiperInstance) {
+      console.log(swiperInstance);
+      swiperInstance.setTransition(0);
+
+      swiperInstance.autoplay.stop();
+    }
+  };
+
+  const handleMouseOut = () => {
+    if (swiperInstance) {
+      swiperInstance.autoplay.start();
+    }
+  };
+
   return (
     <div>
       <Swiper
-        modules={[Scrollbar, Grid, Autoplay]}
+        modules={[Scrollbar, Grid, Autoplay, Navigation]}
         spaceBetween={10}
         slidesPerView={3.85}
-        grid={{
-          rows: 2,
-          fill: "row",
-        }}
         autoplay={{
-          delay: 0,
           disableOnInteraction: false,
-          pauseOnMouseEnter: false,
+          delay: 0,
         }}
         className="swiper-transition swiper-small"
-        speed={5000}
+        speed={1000}
         scrollbar={{ draggable: false }}
         centeredSlides={true}
         loop={true}
+        onSwiper={(swiper) => setSwiperInstance(swiper)}
         style={{
           paddingBottom: "30px",
           paddingTop: "30px",
         }}
       >
         {slides.map((slide, index) => (
-          <SwiperSlide key={index}>
-            <div className=" h-[240px] w-[420px]">
+          <SwiperSlide
+            key={index}
+            className="h-[240px] w-[420px] brightness-100"
+          >
+            <div
+              className="h-[240px] w-[420px]"
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+            >
               <img
                 src={slide.img}
                 alt={`Slide ${index}`}
